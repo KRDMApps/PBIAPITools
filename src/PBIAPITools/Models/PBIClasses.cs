607d7863aeba3aa72f592b4fd9b290b793b83270
Ken Mears <krdmapps@gmail.com>
2016-05-21 17:49:35 -0600

    clean up

---
+++ src/PBIAPITools/Models/PBIClasses.cs
@@ -0,0 +1,34 @@
+﻿namespace PBIAPITools.Models
+{
+    public class Datasets
+    {
+        public dataset[] value { get; set; }
+    }
+
+    public class dataset
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Tables
+    {
+        public table[] value { get; set; }
+    }
+
+    public class table
+    {
+        public string Name { get; set; }
+    }
+
+    public class Groups
+    {
+        public group[] value { get; set; }
+    }
+
+    public class group
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
